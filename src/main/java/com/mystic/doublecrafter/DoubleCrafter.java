7fa433623fe851846eac4d6da93ae124388b6f6c
Mysticpasta1 <ethan.cantrell@asub.edu>
2021-06-29 17:19:42 -0500

    Start

---
+++ src/main/java/com/mystic/doublecrafter/DoubleCrafter.java
@@ -0,0 +1,10 @@
+package com.mystic.doublecrafter;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DoubleCrafter implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
